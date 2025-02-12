f9b65efd55b1ba984b680a948bea96bb27f3b3f6
root <root@LAPTOP-5FOB9NO7.>
2025-02-12 17:11:55 -0600

    Fix date format and prevent duplicates in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,26 @@
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
+    :release_date => '1988-04-16'},
   {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
+    :release_date => '2018-11-16'},
   {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
+    :release_date => '2019-05-30'},
   {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
+    :release_date => '2021-02-19'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'},
+    :release_date => '2021-08-13'},
   {:title => 'Inception', :rating => 'PG-13',
-    :release_date => '16-Jul-2010'},
+    :release_date => '2010-07-16'},
   {:title => 'Interstellar', :rating => 'PG-13',
-    :release_date => '07-Nov-2014'},
+    :release_date => '2014-11-07'},
   {:title => 'Fight Club', :rating => 'R',
-    :release_date => '15-Oct-1999'}
+    :release_date => '1999-10-15'}
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by!(title: movie[:title]) do |m|
+    m.rating = movie[:rating]
+    m.release_date = movie[:release_date]
+  end
 end
